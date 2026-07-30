1d6df38e8883e01703ba6f11e997fb8f8f325d4c
jalfredobecerra <julian19becerra@gmail.com>
2026-07-30 14:17:06 -0500

    W03 Team Activity: Displaying Service Projects

--- src/models/projects.js
+++ src/models/projects.js
@@ -39,4 +39,46 @@ const getProjectsByOrganizationId = async (organizationId) => {
     return result.rows;
 };
 
-export { getAllProjects, getProjectsByOrganizationId };
+const getUpcomingProjects = async (number_of_projects) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.project_date AS date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_project sp
+        JOIN public.organization o ON sp.organization_id = o.organization_id
+        WHERE sp.project_date >= CURRENT_DATE
+        ORDER BY sp.project_date ASC
+        LIMIT $1;
+    `;
+
+    const result = await db.query(query, [number_of_projects]);
+
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.project_date AS date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_project sp
+        JOIN public.organization o ON sp.organization_id = o.organization_id
+        WHERE sp.project_id = $1;
+    `;
+
+    const result = await db.query(query, [id]);
+
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
